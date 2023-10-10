910ff648a10c043854c07a25195165aa78487f5c
dt19863 <dt19863@bristol.ac.uk>
2023-10-10 10:49:36 +0100

    Add test for prim

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,20 +1,15 @@
 test_that("distance_matrix works", {
   xs <- c(0,3,3,6)
   ys <- c(0,0,4,0)
-  D <- matrix(rep(0),nrow=4,ncol=4)
-  D[1,2] <- 3
-  D[2,1] <- 3
-  D[1,3] <- 5
-  D[3,1] <- 5
-  D[1,4] <- 6
-  D[4,1] <- 6
-  D[2,3] <- 4
-  D[3,2] <- 4
-  D[2,4] <- 3
-  D[4,2] <- 3
-  D[3,4] <- 5
-  D[4,3] <- 5
+  D <- rbind(c(0,3,5,6),c(3,0,4,3),c(5,4,0,5),c(6,3,5,0))
   expect_equal(distance_matrix(xs,ys), D)
+})
+
+test_that("prim works", {
+  xs <- c(0,3,3,6)
+  ys <- c(0,0,4,0)
+  A <- rbind(c(0,1,0,0),c(1,0,1,1),c(0,1,0,0),c(0,1,0,0))
+  expect_equal(prim(xs,ys), A)
 })
 
 test_that("graph_plot returns an error if dimensions don't match", {
